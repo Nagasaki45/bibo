970b799f4f695753bc4140f064637ef2d32c9049
Tom Gurion <nagasaki45@gmail.com>
2018-11-26 22:17:46 +0000

    No need to include_package_data in setup.py

--- setup.py
+++ setup.py
@@ -77,9 +77,6 @@ setup(
         'pyperclip',
     ],
 
-    # To include the templates and static files in the distribution.
-    include_package_data=True,
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
