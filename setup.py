2bbf83d09d9ece4c1c49e4b5680a313ecab33019
Tom Gurion <nagasaki45@gmail.com>
2020-03-27 13:00:37 +0300

    Bump version: 0.0.24 → 0.0.25

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='bibo',
-    version='0.0.24',
+    version='0.0.25',
     description='Command line reference manager with single source of truth: the .bib file. Inspired by beets',
     long_description=long_description,
     url='https://github.com/Nagasaki45/bibo',
