8246deeed846cca5c8268583610d0a094906506a
Tom Gurion <nagasaki45@gmail.com>
2020-03-29 23:05:55 +0300

    Bump version: 0.0.26 → 0.0.27

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Tom Gurion"
 author = "Tom Gurion"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.26"
+release = "0.0.27"
 
 
 # -- General configuration ---------------------------------------------------
