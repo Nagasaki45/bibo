8f0f44b5404995d530894ff4657bd40c5cda567b
Tom Gurion <nagasaki45@gmail.com>
2020-05-28 16:56:43 +0300

    Bump version: 0.0.32 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Tom Gurion"
 author = "Tom Gurion"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.32"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
